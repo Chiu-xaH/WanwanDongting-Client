b5dcec4fede7527a7ee9b80033615b6372e6f181
Chiu <zsh0908@outlook.com>
2023-12-07 22:44:01 +0800

    准备开工

---
+++ app/src/main/java/com/hfut/qqmusictp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hfut.qqmusictp
+
+import android.os.Bundle
+import android.view.Window
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.hfut.qqmusictp.ui.TransparentSystemBars
+import com.hfut.qqmusictp.ui.composeUI.MainUI
+import com.hfut.qqmusictp.ui.theme.QQMusicTPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            QQMusicTPTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TransparentSystemBars()
+                    MainUI()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/hfut/qqmusictp/ui/composeUI/MainUI.kt
@@ -0,0 +1,36 @@
+package com.hfut.qqmusictp.ui.composeUI
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainUI() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text("QQ音乐 TP") }
+            )
+        },) {innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding).verticalScroll(rememberScrollState()).fillMaxSize()){
+
+        }
+    }
+    }
